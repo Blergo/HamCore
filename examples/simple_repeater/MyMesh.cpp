a09582c6dbfe87d8e67faaac9b88ad16c69127c0
Blergo <beardedowen@gmail.com>
2026-08-18 21:52:20 +0100

    Update LoRa configuration parameters

--- examples/simple_repeater/MyMesh.cpp
+++ examples/simple_repeater/MyMesh.cpp
@@ -4,13 +4,13 @@
 /* ------------------------------ Config -------------------------------- */
 
 #ifndef LORA_FREQ
-  #define LORA_FREQ 915.0
+  #define LORA_FREQ 434.150
 #endif
 #ifndef LORA_BW
-  #define LORA_BW 250
+  #define LORA_BW 62.5
 #endif
 #ifndef LORA_SF
-  #define LORA_SF 10
+  #define LORA_SF 8
 #endif
 #ifndef LORA_CR
   #define LORA_CR 5
